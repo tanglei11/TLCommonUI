4a420ea78b6ba48577c218c9274aa445f08f865b
汤磊 <13123315202@163.com>
2022-01-29 18:03:49 +0800

    提交说明

---
+++ TLCommonUI/Classes/FormView.h
@@ -0,0 +1,16 @@
+//
+//  FormView.h
+//  PriviteDemo
+//
+//  Created by Peanut on 2022/1/29.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FormView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TLCommonUI/Classes/FormView.m
@@ -0,0 +1,14 @@
+//
+//  FormView.m
+//  PriviteDemo
+//
+//  Created by Peanut on 2022/1/29.
+//
+
+#import "FormView.h"
+
+@implementation FormView
+
+
+
+@end
